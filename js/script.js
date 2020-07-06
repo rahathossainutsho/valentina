acfd7fc2c160e32e9a61260f0e2d53db38476aa6
MMrhuutsho2128 <rahathossainutsho21a@gmail.com>
2020-07-07 02:41:23 +0600

    Valentina fully responsive html template done

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+$('.per').slick({
+    dots:false,
+    infinite: true,
+    speed: 700,
+    autoplay: true,
+    autoplaySpeed: 700,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+
+var countDownDate = new Date("Aug 20, 2020 23:59:59").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("day").innerHTML = days
+  document.getElementById("hour").innerHTML = hours
+  document.getElementById("min").innerHTML = minutes
+  document.getElementById("sec").innerHTML = seconds
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
